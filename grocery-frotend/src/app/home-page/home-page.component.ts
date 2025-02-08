23ee3633ab951ce81eab122b0da721027b91ba61
Gareth M. <gkm25@protonmail.com>
2025-02-08 00:20:59 -0600

    local interface address

--- grocery-frotend/src/app/home-page/home-page.component.ts
+++ grocery-frotend/src/app/home-page/home-page.component.ts
@@ -22,7 +22,7 @@ export class HomePageComponent {
   }
 
   async getGroceryItems() {
-    const resp = await fetch("http://ec2-3-144-83-59.us-east-2.compute.amazonaws.com:5000/get-items")
+    const resp = await fetch("http://172.31.1.240:5000/get-items")
     this.all_items = await resp.json()
     console.log(this.all_items)
   }
@@ -42,7 +42,7 @@ export class HomePageComponent {
       costfield = this.cost_search
     }
     
-    const r = await fetch("http://ec2-3-144-83-59.us-east-2.compute.amazonaws.com:5000/search-items", {
+    const r = await fetch("http://172.31.1.240:5000/search-items", {
       method: "POST",
       body: JSON.stringify({
         name: namefield,
